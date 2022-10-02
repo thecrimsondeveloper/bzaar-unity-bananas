3ff9c8109be069b1c22e131ea31955baafccea25
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2022-10-02 15:37:12 -0700

    echo testing

--- Bzaar/Assets/Bzaar-Demo/Crimson/03 Scripts/Controllers/Echo.cs
+++ Bzaar/Assets/Bzaar-Demo/Crimson/03 Scripts/Controllers/Echo.cs
@@ -6,7 +6,9 @@ using UnityEngine.Networking;
 
 public class Echo : MonoBehaviour
 {
-    //public Echo3DService service;
+    public GameObject EchoHologramPrefab;
+
+    public Echo3DService service;
     //// Start is called before the first frame update
     //void Start()
     //{
@@ -30,6 +32,27 @@ public class Echo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SpawnEchoHologram("6fe6f6bb-a772-46f1-aecd-6b35e571e541");
+        }
     }
+
+
+    public void SpawnEchoHologram(string entryID)
+    {
+        GameObject obj = Instantiate(EchoHologramPrefab,Vector3.zero,Quaternion.identity);
+        obj.GetComponent<Echo3DHologram>().entries = entryID;
+        StartCoroutine(Late_SpawnEchoHologram(obj));
+    }
+
+    IEnumerator Late_SpawnEchoHologram(GameObject spawnedHologram)
+    {
+        yield return new WaitUntil(() => spawnedHologram.GetComponentsInChildren<MeshRenderer>().Length > 0);
+
+        GameObject obj = spawnedHologram.GetComponentsInChildren<MeshRenderer>()[0].gameObject;
+        obj.transform.parent = null;
+        //Destroy(spawnedHologram);
+    }
+
 }
