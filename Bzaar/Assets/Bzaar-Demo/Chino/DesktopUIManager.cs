e3fd2e4ffffc8d52c1a44f816b3790cf07038f51
chinoguerrero <79092132+chinoguerrero@users.noreply.github.com>
2022-10-26 23:19:30 -0400

    Added basic functions to all permanent buttons

--- Bzaar/Assets/Bzaar-Demo/Chino/DesktopUIManager.cs
+++ Bzaar/Assets/Bzaar-Demo/Chino/DesktopUIManager.cs
@@ -5,9 +5,22 @@ using UnityEngine;
 public class DesktopUIManager : MonoBehaviour
 {
     [SerializeField] GameObject propertiesPnl;
+    [SerializeField] GameObject colorsPnl;
 
     public void HomeButtonClick() {
        Debug.Log("HomeButtonClicked");
+    }
+
+    public void DownloadButtonClick() {
+       Debug.Log("DownloadButtonClicked");
+    }
+
+    public void MoveButtonClick() {
+       Debug.Log("MoveButtonClicked");
+    }
+
+    public void ScaleButtonClick() {
+       Debug.Log("ScaleButtonClicked");
     }
 
     public void PropertiesButtonClick() {
@@ -20,4 +33,40 @@ public class DesktopUIManager : MonoBehaviour
             propertiesPnl.SetActive(true);
         }
     }
+
+    public void ColorsButtonClick() {
+       Debug.Log("ColorsButtonClicked");
+
+       if(colorsPnl.activeSelf) {
+            colorsPnl.SetActive(false);
+        }
+        else {
+            colorsPnl.SetActive(true);
+        }
+    }
+
+    public void HideButtonClick() {
+       Debug.Log("HideButtonClicked");
+    }
+
+    public void DeleteButtonClick() {
+       Debug.Log("DeleteButtonClicked");
+    }
+
+    public void ScreenshotButtonClick() {
+       Debug.Log("ScreenshotButtonClicked");
+    }
+
+    public void SaveButtonClick() {
+       Debug.Log("SaveButtonClicked");
+    }
+
+    public void SettingsButtonClick() {
+       Debug.Log("SettingsButtonClicked");
+    }
+
+    public void VolumeButtonClick() {
+       Debug.Log("VolumeButtonClicked");
+    }
+
 }
